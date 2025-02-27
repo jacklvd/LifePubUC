e72e4ecd1f68b0e844eee743a1da8ee56a9fdd1b
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2025-02-26 22:07:05 -0500

    fix the conflict bug

--- app/client/app/(dashboard)/layout.tsx
+++ app/client/app/(dashboard)/layout.tsx
@@ -4,15 +4,9 @@ import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
 const LayoutPage = async ({ children }: { children: React.ReactNode }) => {
-<<<<<<< HEAD
-  // const session = await auth();
-
-  // if (!session) redirect("/sign-in");
-=======
   const session = await auth()
 
   if (!session) redirect('/sign-in')
->>>>>>> master
 
   return (
     <div className="flex-col w-full min-h-full">
