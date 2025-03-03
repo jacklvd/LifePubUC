4ae00a32585a38527a19abd86b0ea7dc3c881357
jacklvd <vodanglong3103@gmail.com>
2025-03-02 21:07:29 -0500

    added back missing file during merge conflict

---
+++ app/client/app/organization/layout.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react'
+import Navbar from '@/components/navbar'
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation'
+import OrganizationSideBar from '@/components/organization-sidebar'
+
+const Layout = async ({ children }: { children: ReactNode }) => {
+    const session = await auth()
+
+    if (!session) redirect('/sign-in')
+    return (
+        <div className="flex-col w-full min-h-full">
+            <Navbar />
+            <main>
+                <div className="flex h-screen bg-white">
+                    <OrganizationSideBar />
+                    {children}
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Layout
