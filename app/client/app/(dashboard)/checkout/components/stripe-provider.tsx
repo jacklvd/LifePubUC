f95a79f2e0d4a13d56cf48a24812536c45d48d06
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2025-03-09 00:34:08 -0500

    add initial payment setup

--- app/client/app/(dashboard)/checkout/components/stripe-provider.tsx
+++ app/client/app/(dashboard)/checkout/components/stripe-provider.tsx
@@ -1,6 +1,6 @@
 import { Appearance, loadStripe, StripeElementsOptions } from '@stripe/stripe-js'
 import { useEffect, useState } from "react";
-import { Elements } from '@stripe/react-stripe-js';
+import { EmbeddedCheckoutProvider } from '@stripe/react-stripe-js';
 import { createStripePaymentIntent } from '@/lib/actions/stripe-actions';
 
 import { useCartStore } from '@/store/cart';
@@ -10,27 +10,11 @@ if (!process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY) {
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
-const appearance: Appearance = {
-    theme: "stripe",
-    variables: {
-        colorPrimary: "#0570de",
-        colorBackground: "#18181b",
-        colorText: "#d2d2d2",
-        colorDanger: "#df1b41",
-        colorTextPlaceholder: "#6e6e6e",
-        fontFamily: "Inter, system-ui, sans-serif",
-        spacingUnit: "3px",
-        borderRadius: "10px",
-        fontSizeBase: "14px",
-    },
-};
-
 const StripeProvider = ({ children }: { children: React.ReactNode }) => {
     const [clientSecret, setClientSecret] = useState<string>("");
     const totalAmount = useCartStore((state) => state.totalAmount);
     const items = useCartStore((state) => state.items);
 
-    // Need to create payment intent here
     useEffect(() => {
         
         const fetchPaymentIntent = async () => {
@@ -38,17 +22,12 @@ const StripeProvider = ({ children }: { children: React.ReactNode }) => {
             const response = await createStripePaymentIntent(
                 { cartItems: items }
             )
-
-
-            console.log(response);
+            // console.log(response);
+            
             if (response.success) {
 
                 setClientSecret(response.data)
             }
-
-
-
-
         }
 
         fetchPaymentIntent()
@@ -64,13 +43,13 @@ const StripeProvider = ({ children }: { children: React.ReactNode }) => {
 
     const options: StripeElementsOptions = {
         clientSecret,
-        appearance
+        
     }
 
     return (
-        <Elements stripe={stripePromise} options={options}>
+        <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
             {children}
-        </Elements>
+        </EmbeddedCheckoutProvider>
     )
 }
 
