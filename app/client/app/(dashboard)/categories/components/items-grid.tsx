cc78b56390192c103043c52a8be7606f64bc51c1
BinniesLite <89041460+BinniesLite@users.noreply.github.com>
2025-03-24 15:41:57 -0400

    create individual item page and fix item creation error

--- app/client/app/(dashboard)/categories/components/items-grid.tsx
+++ app/client/app/(dashboard)/categories/components/items-grid.tsx
@@ -4,8 +4,13 @@ import React from 'react'
 import ItemCard from './item-card'
 import { useCartStore } from '@/store/cart'
 
-const ItemsGrid = ({ items, onItemSelect, onAddToCart }) => {
-  const addItem = useCartStore((state) => state.addItem)
+interface ItemGridProps {
+  items: Item[],
+  onAddToCart: (item: Item) => void
+}
+
+
+const ItemsGrid = ({ items, onAddToCart }: ItemGridProps) => {
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
@@ -14,7 +19,7 @@ const ItemsGrid = ({ items, onItemSelect, onAddToCart }) => {
           key={index}
           className="transition-all duration-200 hover:scale-[1.02]"
         >
-          <ItemCard item={item} addItem={addItem} />
+          <ItemCard item={item} addItem={onAddToCart} />
         </div>
       ))}
     </div>
