fc0e7575464a9630c665a475d23bfe3c2107278d
jacklvd <vodanglong3103@gmail.com>
2025-03-24 20:04:37 -0400

    clean up

--- app/client/app/organization/events/[eventId]/ticket/components/empty-ticket-state.tsx
+++ app/client/app/organization/events/[eventId]/ticket/components/empty-ticket-state.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Card, CardContent } from '@/components/ui/card'
 import { Button } from '@/components/ui/button'
-import { Icon } from '../icons'
+import { Icon } from '../../../../../../components/icons'
 
 interface EmptyTicketStateProps {
   onAddClick: () => void
